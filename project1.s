845f8c935b2e648ddd149d24ce6a90f7a57db621
chadillejones <chadille.jones@bison.howard.edu>
2019-10-30 13:32:05 -0400

    MIPSCommit

--- project1.s
+++ project1.s
@@ -4,6 +4,9 @@ userInput .space 10 #stores the user input(10 chars)
 .text #Assembly language instruction
 
 main:
+
+	li $v0, 8 #gets user input  
+	
 	#tell the system this is the end of file
 	li $v0, 10 
 	syscall
