e11f25e8f95fdee09f4a7d6719ae04e7af998b3f
chadillejones <chadille.jones@bison.howard.edu>
2019-10-30 13:33:41 -0400

    MIPSCommit

--- project1.s
+++ project1.s
@@ -5,7 +5,8 @@ userInput .space 10 #stores the user input(10 chars)
 
 main:
 
-	li $v0, 8 #gets user input  
+	li $v0, 8 #gets user input 
+	la $a0, userInput #sets it to the alloted variable 
 	
 	#tell the system this is the end of file
 	li $v0, 10 
