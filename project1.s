66a5abb8c05e550179a798be22e2beb792523bba
chadillejones <chadille.jones@bison.howard.edu>
2019-10-30 16:11:54 -0400

    Initialized registers to hold non summed values

--- project1.s
+++ project1.s
@@ -12,9 +12,13 @@ main:
 	
 	la $t0, userInput #loads the string address in $t0
 	li $t1, 0 #initialized the sum of all the characters
+	li $t2, 32 #initialized a space to check for spaces
+	
 			 
 loop: #to loop through the characters of the string
 	lb $t2, 0($t0) #takes one character in the string
+	
+	
 	
 	#tell the system this is the end of file
 	li $v0, 10 
