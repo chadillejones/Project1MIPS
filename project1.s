972c16c86ed2ce775179b2502a6d380fa2bc76bc
chadillejones <chadille.jones@bison.howard.edu>
2019-10-30 13:44:36 -0400

    MIPSCommit

--- project1.s
+++ project1.s
@@ -7,12 +7,10 @@ main:
 
 	li $v0, 8 #gets user input 
 	la $a0, userInput #sets it to the alloted variable
-	li $a1, 10 #specify the length of the input
+	li $a1, 11 #specify the length of the input
 	syscall
 	
-	 li $v0, 4
-	 la $a0, userInput
-	 syscall
+	 
 	#tell the system this is the end of file
 	li $v0, 10 
 	syscall
